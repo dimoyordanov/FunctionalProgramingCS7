f2b59238fe2e034f70b632f55384689cf365236a
dimoy <75063792+dimoyordanov@users.noreply.github.com>
2025-10-06 12:40:04 +0300

    Create ex7.scm

---
+++ week1/solution/ex7.scm
@@ -0,0 +1,26 @@
+#lang racket
+(define (succ x) (+ 1 x))
+
+(define (pred x) (+ -1 x))
+
+(define (repeat n f x) (
+    if (= n 0)
+        x
+        (repeat (- n 1) f (f x))
+))
+
+(define (_+_ a b) (if (zero? b) a (_+_ (succ a) (pred b))))
+(define (_+_2 a b) (repeat a succ b))
+(define (_-_ a b) (if (zero? b) a (_-_ (pred a) (pred b))))
+(define (_-_2 a b) (repeat a pred b))
+(define (_*__ a b c) (if (zero? b) 0 (if (zero? (pred b)) a (_*__ (_+_ a c) (pred b) c))))
+(define (_*_ a b) (_*__ a b a))
+
+(define (pl a) 
+    (define (pl2 b) (_+_ a b))
+    pl2
+)
+
+(define (_*_2 a b) (_-_ (repeat a (pl b) b) b))
+
+(define (_/_ a b) (if (zero? b) #f (if (< (_-_ a b) 0) 0 (+ 1 (_/_ (_-_ a b) b)))))
