e06df1a40c662665c77d5b1179843f3c3772a657
dimoy <75063792+dimoyordanov@users.noreply.github.com>
2025-11-11 10:06:39 +0200

    Add stream functions for recursive calculations

---
+++ week6/solutions/ex3.scm
@@ -0,0 +1,15 @@
+(define (_stream f i) (cons (f i) (lambda () (_stream f (+ 1 i)) )))
+
+(define (_stream2 f i ai-1)
+  (define (_stream2_ f i ai-1) (cons (f i ai-1) (lambda () (_stream2_ f (+ 1 i) (f i ai-1)) )))
+  (cons ai-1 (lambda () (_stream2_ f i ai-1)))
+)
+
+(define  (_stream3 f i ai-1 ai-2)
+  (define (_stream3_ f i ai-1 ai-2) (cons (f i ai-1 ai-2) (lambda () (_stream3_ f (+ 1 i) (f i ai-1 ai-2) ai-1) )))
+  (cons ai-1 (lambda () (cons ai-2 (lambda () (_stream3_ f i ai-1 ai-2)))))
+)
+
+(define stream (_stream (lambda (x) x) 0))
+
+(define factoriel (_stream2 (lambda (a b) (* a b)) 1 1))
